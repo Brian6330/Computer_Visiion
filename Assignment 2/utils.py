126613b9fefaacdbe1f4d64c40f1e4b5392afdb7
Brian6330 <games@schweigler.org>
2021-11-25 14:54:14 +0100

    A02: Exercise done

--- Assignment 2/utils.py
+++ Assignment 2/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.linalg import null_space
 
 
 def get_normalization_matrix(x):
@@ -13,15 +14,30 @@ def get_normalization_matrix(x):
     # 
     # Output: T 3x3 transformation matrix of points
 
-    # TODO TASK:
     # --------------------------------------------------------------
     # Estimate transformation matrix used to normalize
     # the inputs x
     # --------------------------------------------------------------
 
     # Get centroid and mean-distance to centroid
+    _T = np.zeros((3, 3))
+    centroid = np.mean(x, 1)
+    s = 1 / np.sqrt(
+        np.sum(
+            np.square(
+                np.apply_along_axis(lambda v: v - centroid, 0, x)
+            )
+        )
+        / (2 * x.shape[1])
+    )
 
-    return T
+    _T[0, 0] = s
+    _T[0, 2] = - s * centroid[0]
+    _T[1, 1] = s
+    _T[1, 2] = - s * centroid[1]
+    _T[2, 2] = 1
+
+    return _T
 
 
 def eight_points_algorithm(x1, x2, normalize=True):
@@ -33,49 +49,72 @@ def eight_points_algorithm(x1, x2, normalize=True):
     Outputs:
                     F       3x3     fundamental matrix
     """
-    N = x1.shape[1]
+    _N = x1.shape[1]
 
     if normalize:
         # Construct transformation matrices to normalize the coordinates
-        # TODO
+        _Tx1 = get_normalization_matrix(x1)
+        _Tx2 = get_normalization_matrix(x2)
 
         # Normalize inputs
-        # TODO
-        pass
+        n1 = np.apply_along_axis(lambda x: np.matmul(_Tx1, x), 0, x1)
+        n2 = np.apply_along_axis(lambda x: np.matmul(_Tx2, x), 0, x2)
 
     # Construct matrix A encoding the constraints on x1 and x2
-    # TODO
+    _A = np.zeros((_N, 9))
+    for i in range(_N):
+        _A[i, 0] = n2[0, i] * n1[0, i]
+        _A[i, 1] = n2[0, i] * n1[1, i]
+        _A[i, 2] = n2[0, i]
+        _A[i, 3] = n2[1, i] * n1[0, i]
+        _A[i, 4] = n2[1, i] * n1[1, i]
+        _A[i, 5] = n2[1, i]
+        _A[i, 6] = n1[0, i]
+        _A[i, 7] = n1[1, i]
+        _A[i, 8] = 1
 
-    # Solve for f using SVD
-    # TODO
+    # Solve for _F using SVD
+    u, s, v = np.linalg.svd(_A)
+    _F = v.T[:, 8].reshape(3, 3)
 
-    # Enforce that rank(F)=2
-    # TODO
+    # Enforce that rank(_F)=2
+    u, s, v = np.linalg.svd(_F)
 
+    # Remove smallest singular value as described on slide 33 in lecture 08a_epipolar
+    s_temp = np.array([[s[0], 0, 0],
+                       [0, s[1], 0],
+                       [0, 0, 0]])
+    _F = np.matmul(np.matmul(u, s_temp), v)
+
+    # Transform F back
     if normalize:
-        # Transform F back
-        # TODO
-        pass
+        _F = np.matmul(np.matmul(_Tx2.T, _F), _Tx1)
 
-    return F
+    return _F
 
 
-def right_epipole(F):
+def right_epipole(_F):
     """
     Computes the (right) epipole from a fundamental matrix F.
     (Use with F.T for left epipole.)
+    Description of epipole in Jupyter Notebook.
     """
 
     # The epipole is the null space of F (F * e = 0)
-    # TODO
+    e = null_space(_F)
+    return e / e[2]
 
-    return e
 
-
-def plot_epipolar_line(im, F, x, e):
+def plot_epipolar_line(im, _F, x, e, plot=plt):
     """
     Plot the epipole and epipolar line F*x=0 in an image. F is the fundamental matrix
     and x a point in the other image.
+    Description of epipolar lines in Jupyter Notebook
     """
     m, n = im.shape[:2]
-    # TODO
+    epipolar_line = np.dot(_F, x)
+
+    x = np.linspace(0, n, 4)
+    y = np.array([(epipolar_line[2] + epipolar_line[0] * x_coord) / (-epipolar_line[1]) for x_coord in x])
+
+    plot.plot(x[(y >= 0) & (y < m)], y[(y >= 0) & (y < m)], linewidth=2)
